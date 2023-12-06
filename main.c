0d6320d4f509d69a33f51d06a809823d5844601b
João Pedro <joaopdesousa12@gmail.com>
2023-12-06 20:59:36 -0300

    Alterações em buscarTarefas: Inclusão de ponteiros e validação de campos.

--- main.c
+++ main.c
@@ -1,16 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-// Crie um sistema que permita o cadastro de tarefas. O menu deve permitir as seguintes ações:
-//  1. Inserir nova tarefa; (Daniel)
-//  2. Mostrar tarefas cadastradas (Gustavo)
-//  3. Buscar por tarefa; (João)
-//  4. Editar informações de uma tarefa; (Erik)
-//  5. Remover tarefa; (Viktor)
-//  6. Marcar conclusão de uma tarefa específica; (Viktor)
-//  7. Salvar lista de tarefas em um arquivo, incluindo se cada uma foi finalizada ou não (Erik)
-
-// Separe 
-
 
 typedef struct { // <----------------------- struct com as propriedades de cada tarefa
     int posicao;
@@ -19,6 +8,21 @@ typedef struct { // <----------------------- struct com as propriedades de cada 
     char estado[9]; //Pendente, Fazendo, Concluido
 
 } Tarefa;
+
+void exibirMenu(){
+    printf("--------------------------------------------------------------\n");
+    printf("ESCOLHA UMA DAS OPCOES ABAIXO: \n\n");
+    printf("1. Inserir nova tarefa.\n");
+    printf("2. Mostrar tarefas cadastradas.\n");
+    printf("3. Buscar por tarefa.\n");
+    printf("4. Editar informações de uma tarefa.\n");
+    printf("5. Remover tarefa.\n");
+    printf("6. Marcar conclusao de uma tarefa especifica.\n");
+    printf("7. Salvar lista de tarefas em um arquivo.\n");
+    printf("8. Finalizar do programa.\n");
+    
+    printf("--------------------------------------------------------------\n");
+}
 
 void inserirTarefa(Tarefa lista[], int totalTarefas) {
     printf("Digite o titulo da tarefa: ");
@@ -46,152 +50,178 @@ void mostrarTarefas(Tarefa lista[], int totalTarefas) {
     }
 }
 
-Tarefa buscarTarefaPorTitulo(Tarefa listaTarefas[], int length, char titulo[25]){
+void buscarTarefaPorTitulo(Tarefa *listaTarefas, int length, char *titulo) {
+    int resultado = 0;
+    printf("--------------------------------------------------------------\n");
 
-    for (int i=0;i<length;i++){
-        if(strcmp(titulo, listaTarefas[i].titulo) == 0){
-            printf("--------------------------------------------------------------\n");
+    for (int i = 0; i < length; i++) {
+        if (strcmp(titulo, listaTarefas[i].titulo) == 0) {
+            printf("RESULTADO:\n\n");
 
-            printf("Posicao: %d\n",listaTarefas[i].posicao);
-            printf("Titulo: %s\n",listaTarefas[i].titulo);
-            printf("Descricao: %s\n",listaTarefas[i].descricao);
-            printf("estado: %s\n",listaTarefas[i].estado);
-        
-            printf("--------------------------------------------------------------\n");
+            printf("Posicao: %d\n", listaTarefas[i].posicao);
+            printf("Titulo: %s\n", listaTarefas[i].titulo);
+            printf("Descricao: %s\n", listaTarefas[i].descricao);
+            printf("Estado: %s\n", listaTarefas[i].estado);
 
-            return listaTarefas[i];
+            resultado = 1;
+            break;
         }
     }
+
+    if (!resultado) {
+        printf("Nenhuma tarefa encontrada com o titulo %s.\n", titulo);
+    }
     
+    printf("--------------------------------------------------------------\n");
+
 }
 
-Tarefa buscarTarefaPorPosicao(Tarefa listaTarefas[], int length, int posicao){
+void buscarTarefaPorPosicao(Tarefa *listaTarefas, int length, int posicao) {
+    int resultado = 0;
+    printf("--------------------------------------------------------------\n");
 
-    for (int i=0;i<length;i++){
-        if(posicao == listaTarefas[i].posicao){
+    for (int i = 0; i < length; i++) {
+        if (posicao == listaTarefas[i].posicao) {
             printf("--------------------------------------------------------------\n");
-
-            printf("Posicao: %d\n",listaTarefas[i].posicao);
-            printf("Titulo: %s\n",listaTarefas[i].titulo);
-            printf("Descricao: %s\n",listaTarefas[i].descricao);
-            printf("estado: %s\n",listaTarefas[i].estado);
-        
+            printf("RESULTADO:\n\n");
+            
+            printf("Posicao: %d\n", listaTarefas[i].posicao);
+            printf("Titulo: %s\n", listaTarefas[i].titulo);
+            printf("Descricao: %s\n", listaTarefas[i].descricao);
+            printf("Estado: %s\n", listaTarefas[i].estado);
             printf("--------------------------------------------------------------\n");
             
-            return listaTarefas[i];
+            resultado = 1;
+            break;
         }
     }
+
+    if (!resultado) {
+        printf("Nenhuma tarefa encontrada na posicao %d.\n", posicao);
+    }
+
+    printf("--------------------------------------------------------------\n");
+
 }
-// teste
+
 int main() {
     int numero;
-    // Menu inicial
-    printf("--------------------------------------------------------------\n");
-
-    printf("1. Inserir nova tarefa.\n");
-    printf("2. Mostrar tarefas cadastradas.\n");
-    printf("3. Buscar por tarefa.\n");
-    printf("4. Editar informações de uma tarefa.\n");
-    printf("5. Remover tarefa.\n");
-    printf("6. Marcar conclusao de uma tarefa especifica.\n");
-    printf("7. Salvar lista de tarefas em um arquivo.\n\n");
-    
-    printf("--------------------------------------------------------------\n");
-
     Tarefa listaTarefas[] = {
         {0, "oi", "teste", "Pendente"},
         {1, "Oi2", "teste2", "Fazendo"},
         {2, "Oi3", "teste3", "Concluido"}
     };
 
-    
-    printf("Digite um número para realizar alguma ação: ");
-    scanf("%d", &numero);
+    while(numero != 8){
 
-    //Estrutura básica do documento
+        //Exibe menu inicial
+        exibirMenu();
+        
+        printf("Digite um numero para realizar alguma acao: \n");
+        scanf("%d", &numero);
 
-    switch(numero) {
-        case 1: //(Daniel)
-            printf("1. Inserir nova tarefa;\n");
+        //Finalizar execucao do script
+        if (numero == 8){
+            printf("Programa sendo finalizado...\n");
             break;
+        }
 
-        case 2: //(Daniel)
-            printf("2. Mostrar tarefas cadastradas;\n");
-            break;
+        //Estrutura basica do documento
+        switch(numero) {
+            case 1:
+                printf("1. Inserir nova tarefa;\n");
+                break;
 
-        case 3: //(João)
-            int resposta;
-            int length = sizeof(listaTarefas) / sizeof(listaTarefas[0]);
+            case 2: 
+                printf("2. Mostrar tarefas cadastradas;\n");
+                break;
 
-            printf("Buscar por tarefa por titulo ou posicao?\n");
-            printf("0 - Titulo\n");
-            printf("1 - Posicao\n");
-            scanf("%d",&resposta);
+            case 3: 
+                int resposta;
+                int length = sizeof(listaTarefas) / sizeof(listaTarefas[0]);
 
-            if(resposta == 0){
-                char titulo[25];
-                printf("Qual o titulo da tarefa que você deseja buscar? ");
-                scanf("%s",&titulo);
-                buscarTarefaPorTitulo(listaTarefas, length, titulo);
-            }
-            
-            else if(resposta == 1){
-                int posicao;
-                printf("Qual a posicao da tarefa que você deseja buscar? ");
-                scanf("%d",&posicao);
-                printf("\n");
-                buscarTarefaPorPosicao(listaTarefas, length, posicao);
-            }
+                printf("Escolha uma das opcoes abaixo para buscar tarefas por titulo ou posicao:\n");
+                printf("0 - Titulo\n");
+                printf("1 - Posicao\n");
+                scanf("%d",&resposta);
 
-            else{
-                printf("Não existe nenhum titulo ou posicao com esse valor\n");
-            }
+                getchar();
 
-            break;
+                if(resposta == 0){
+                    char titulo[25];
 
-        case 4: //(Erik)
-            int respostaEdicao;
-            int lengthEdicao = sizeof(listaTarefas) / sizeof(listaTarefas[0]);
+                    printf("Qual o titulo da tarefa que você deseja buscar?\n");
 
-            printf("Buscar por tarefa por titulo ou posicao?\n");
-            printf("0 - Titulo\n");
-            printf("1 - Posicao\n");
-            scanf("%d",&respostaEdicao);
+                    if(fgets(titulo, sizeof(titulo), stdin) != NULL){
+                        titulo[strcspn(titulo, "\n")] = '\0'; // Remover o caractere de nova linha
+                        buscarTarefaPorTitulo(listaTarefas, length, titulo);
+                    }
 
-            if(resposta == 0){
-                char titulo[25];
-                printf("Qual o titulo da tarefa que você deseja buscar? ");
-                scanf("%s",&titulo);
-                buscarTarefaPorTitulo(listaTarefas, lengthEdicao, titulo);
-            }
-            
-            else if(resposta == 1){
-                int posicao;
-                printf("Qual a posicao da tarefa que você deseja editar? ");
-                scanf("%d",&posicao);
-                printf("\n");
-                editarTarefaPosicao(listaTarefas, length, posicao);
-            }
+                    else{
+                        printf("Por favor, insira um titulo valido\n");
+                    }
 
-            else{
-                printf("Não existe nenhum titulo ou posicao com esse valor\n");
-            }
-            break;
+                }
+                
+                else if(resposta == 1){
+                    int posicao;
 
-        case 5: //(Viktor)
-            printf("Remover tarefa;\n");
-            break;
+                    printf("Qual a posicao da tarefa que você deseja buscar? \n");
+                    scanf("%d",&posicao);
 
-        case 6: //(Viktor)
-            printf("Marcar conclusão de uma tarefa específica;\n");
-            break;
+                    buscarTarefaPorPosicao(listaTarefas, length, posicao);
+                }
 
-        case 7: //(Erik)
-            printf("Salvar lista de tarefas em um arquivo\n");
-            break;
+                else{
+                    printf("Comando invalido. Tente novamente!\n");
+                    continue;
+                }
+                break;
 
-        default:
-            printf("Número inválido, tente novamente! \n");
+            case 4:
+                int respostaEdicao;
+                int lengthEdicao = sizeof(listaTarefas) / sizeof(listaTarefas[0]);
+
+                printf("Buscar por tarefa por titulo ou posicao?\n");
+                printf("0 - Titulo\n");
+                printf("1 - Posicao\n");
+                scanf("%d",&respostaEdicao);
+
+                if(resposta == 0){
+                    char titulo[25];
+                    printf("Qual o titulo da tarefa que você deseja buscar? ");
+                    scanf("%s",&titulo);
+                    buscarTarefaPorTitulo(listaTarefas, lengthEdicao, titulo);
+                }
+                
+                else if(resposta == 1){
+                    int posicao;
+                    printf("Qual a posicao da tarefa que você deseja editar? ");
+                    scanf("%d",&posicao);
+                    printf("\n");
+                    editarTarefaPosicao(listaTarefas, length, posicao);
+                }
+
+                else{
+                    printf("Não existe nenhum titulo ou posicao com esse valor\n");
+                }
+                break;
+
+            case 5:
+                printf("Remover tarefa;\n");
+                break;
+
+            case 6:
+                printf("Marcar conclusão de uma tarefa específica;\n");
+                break;
+
+            case 7:
+                printf("Salvar lista de tarefas em um arquivo\n");
+                break;
+
+            default:
+                printf("Número inválido, tente novamente! \n");
+        }
     }
 
     return 0;
